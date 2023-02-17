32d60322f24551a78db279f94cf876475ce6df22
jayasfps <125624996+jayasfps@users.noreply.github.com>
2023-02-17 16:12:06 +1100

    Update streamlit_app.py
    Add smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard boiled free range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
